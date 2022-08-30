092d5a110f9d0c6255703c1ea82233606338dcdd
Monet <shelbyhernandez9876@yahoo.com>
2022-08-30 02:13:21 -0500

    finalized functions in JS file, added extra styling in CSS, as well as extra icons and bootstrap styling in HTML file

--- README.md
+++ README.md
@@ -3,32 +3,26 @@ A work day planner powered by JavaScript
 
 ## Description
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+- This simple workday planner utilizes basic BootStrap components, Font Awesome icons, timers and jQuery to add and save your day to day tasks.
+
+- This project was built to better understand using moment.js, jQuery, and getting and setting local storage items
 
 
+## Demo
 
-## Installation
+- Live URL: (https://shernandez927.github.io/tabulation/)
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    ![screenshotimg](./assets/images/tabulationscreenshot.png)
 
-## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
 
 ## Credits
 
 - Source Code provided by Coding BootCamp at UT Austin:
 (https://github.com/coding-boot-camp/super-disco) 
 
+- Collaborators:
+
+- Salissa Hernandez (https://github.com/Salissa4)
 
 ## License
 
@@ -56,16 +50,5 @@ SOFTWARE.
 
 ## Badges
 
-![reposizebadge](https://img.shields.io/github/repo-size/shernandez927/tabulation?style=for-the-badge) ![licensebadge](https://img.shields.io/github/license/shernandez927/tabulation?style=for-the-badge)
+![reposizebadge](https://img.shields.io/github/repo-size/shernandez927/tabulation?style=for-the-badge) ![licensebadge](https://img.shields.io/github/license/shernandez927/tabulation?style=for-the-badge) ![languagebadge](https://img.shields.io/github/languages/count/shernandez927/tabulation?style=for-the-badge)
 
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- script.js
+++ script.js
@@ -1,3 +1,74 @@
-// Displays current date and time with moment.js
-var current = moment().format("MMM Do, YYYY, h:mm");
+// runs the inner functions once the page DOM is ready
+$(document).ready(function () {
+
+var current = moment().format("MMM Do, YYYY, h:mm:ss");
 $("#currentDay").text(current);
+
+// Function to form "live" clock using a timer interval
+function updateTime() {
+  $('#currentDay').html(moment().format("MMM Do, YYYY, h:mm:ss"));
+}
+setInterval(updateTime, 1000);
+
+// runs the inner functions once the page DOM is ready
+// $(document).ready(function () {
+
+  // event listener to run the function based on the save button click
+$(".saveBtn").on("click", function() { 
+  var task = $(this).siblings(".taskInput").val();
+  var time = $(this).parent().attr("id");
+  const mainContainer = document.getElementById("jumbotron");
+
+// if statement to alert user to save a valid task value otherwise to append h3 with a save confirmation text
+  mainContainer.innerHTML = "";
+  if (task === "") {
+    window.alert("Please enter a task before saving");
+    return;
+  } else {
+    const saveAlert = document.createElement("h3");
+    saveAlert.textContent = "Your task has been saved to Local Storage! ✔️";
+    mainContainer.append(saveAlert);
+
+// saves the task input and timestamp into local storage
+  localStorage.setItem(time, task);
+  }
+  });
+
+// gets items saved from local storage
+$("#hour09 .taskInput").val(localStorage.getItem("hour09"));
+$("#hour10 .taskInput").val(localStorage.getItem("hour10"));
+$("#hour11 .taskInput").val(localStorage.getItem("hour11"));
+$("#hour12 .taskInput").val(localStorage.getItem("hour12"));
+$("#hour13 .taskInput").val(localStorage.getItem("hour13"));
+$("#hour14 .taskInput").val(localStorage.getItem("hour14"));
+$("#hour15 .taskInput").val(localStorage.getItem("hour15"));
+$("#hour16 .taskInput").val(localStorage.getItem("hour16"));
+$("#hour17 .taskInput").val(localStorage.getItem("hour17"));
+
+
+// updates time block color codes with if statements based on current time
+function blockUpdate() {
+  var currentHour = moment().hour();
+
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+
+    }
+  });
+ }
+
+  blockUpdate();
+
+});
